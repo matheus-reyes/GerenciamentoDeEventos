23165559985bb71649da05f44ef8166d58b1a39e
matheus-reyes <matheus-reyes@usp.br>
2020-11-16 01:51:37 -0300

    Início das páginas do participante e da promotora

--- routes/cadastroRouter.js
+++ routes/cadastroRouter.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const controller = require("../controllers/CadastroController");
 
 router.get("/cadastro", controller.cadastro);
+router.post("/cadastroParticipante", controller.cadastroParticipante);
+router.post("/cadastroPromotora", controller.cadastroPromotora);
 
 module.exports = router;
