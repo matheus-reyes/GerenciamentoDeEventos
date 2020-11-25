56b853055426cdaa72d1880149be6d317a753abf
matheus-reyes <matheus-reyes@usp.br>
2020-11-24 21:20:04 -0300

    Login Funcional

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/loginRouter');
@@ -21,6 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret:"GerenciamentoDeEventos",
+  resave:true,
+  saveUninitialized:true
+}));
 
 app.use('/', indexRouter);
 app.use('/', loginRouter);
--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,28 +1,60 @@
-const {Promotora, Participante} = require("../models");
+const {Entidade, Participante} = require("../models");
 const bcrypt = require("bcrypt");
 
 module.exports = {
 
+    //função para carregar a página de login
     login: (req, res) => {
 
         res.render("login");
 
     },
 
+    //função para realizar o login
     loginForm: async (req, res) => {
 
+        //captura os valores do formulário
         let email = req.body.email;
         let senha = req.body.password;
 
+        //encontra todos os participantes
         const participantes = await Participante.findAll();
 
+        //encontra todas as promotoras (Entidades)
+        const promotoras = await Entidade.findAll();
+
+        //percorre os participantes
         for(let i = 0; i < participantes.length; i++){
-            if(participantes[i].email == email){
-                console.log(participantes[i].nome);
-            }            
+
+            //se o e-mail e senha informados batem com os dados de algum participante
+            if(participantes[i].e_mail == email && bcrypt.compareSync(senha, participantes[i].senha)){
+                
+                // Salva as Informações do usuário na session
+                req.session.usuario = participantes[i];
+
+                // Redireciona ao início do participante com seus dados
+                res.render("participante/inicio", {usuario:req.session.usuario});
+
+            }
         }
 
-        // && bcrypt.compareSync(senha, participantes[i].senha)
+        //percorre as promotoras
+        for(let i = 0; i < promotoras.length; i++){
+
+            //se o e-mail e senha informados batem com os dados de algum participante
+            if(promotoras[i].email == email && bcrypt.compareSync(senha, promotoras[i].senha)){
+                
+                // Salva as Informações do usuário na session
+                req.session.usuario = promotoras[i];
+
+                // Redireciona ao início do participante com seus dados
+                res.render("promotora/inicio", {usuario:req.session.usuario});
+
+            }
+        }
+
+        //Se não foi encontrado nenhum usuário, redireciona ao login
+        res.render("login");
 
     }
 
